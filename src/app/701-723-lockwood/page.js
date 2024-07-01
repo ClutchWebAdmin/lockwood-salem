51bca2be1835c73b1ab4fa978e361b83dd9a52ee
Gabe Wolford <gabewolford@gmail.com>
2024-07-01 15:53:07 -0700

    cleaned up layout of detail page

--- src/app/701-723-lockwood/page.js
+++ src/app/701-723-lockwood/page.js
@@ -4,17 +4,17 @@ import { lockwood701 } from "../data/lockwood701";
 export default function Lockwood701Page() {
   return (
     <div className="lg:mt-[var(--desktopHeaderHeight)] p-3">
-      <div className="flex flex-col gap-5 p-3">
+      <section className="flex flex-col gap-5 p-3">
         <h1 className="text-6xl md:text-7xl">701-723 Lockwood</h1>
         <div className="flex flex-row gap-3 font-medium text-primaryLight">
           <p className="bg-secondaryGreen rounded px-2 py-1">1 Bedroom</p>
           <p className="bg-secondaryGreen rounded px-2 py-1">1 Bathroom</p>
           <p className="bg-secondaryGreen rounded px-2 py-1">450 sqft</p>
         </div>
-      </div>
+      </section>
 
       <section className="grid lg:grid-cols-12 gap-10 pt-8 pb-16">
-        <div className="flex flex-col gap-3 p-3 lg:col-span-6">
+        <div className="flex flex-col p-3 lg:col-span-6">
           <h4 className="col-span-full text-3xl pb-4 font-medium">Amenities</h4>
           <ul className="flex flex-wrap gap-3">
             {lockwood701.amenities.map((item, index) => (
@@ -29,10 +29,11 @@ export default function Lockwood701Page() {
             ))}
           </ul>
         </div>
-        <div className="flex flex-col gap-3 lg:col-span-6">
+
+        <section className="flex flex-col lg:col-span-6 p-3">
           <h4 className="col-span-full text-3xl pb-4 font-medium">Photos</h4>
           <Carousel photos={lockwood701.photos} />
-        </div>
+        </section>
       </section>
     </div>
   );
--- src/app/725-lockwood/page.js
+++ src/app/725-lockwood/page.js
@@ -1,20 +1,20 @@
 import Carousel from "../components/Carousel";
 import { lockwood725 } from "../data/lockwood725";
 
-export default function Lockwood725Page() {
+export default function Lockwood701Page() {
   return (
     <div className="lg:mt-[var(--desktopHeaderHeight)] p-3">
-      <div className="flex flex-col gap-5 p-3">
+      <section className="flex flex-col gap-5 p-3">
         <h1 className="text-6xl md:text-7xl">725 Lockwood</h1>
         <div className="flex flex-row gap-3 font-medium text-primaryLight">
           <p className="bg-secondaryGreen rounded px-2 py-1">1 Bedroom</p>
           <p className="bg-secondaryGreen rounded px-2 py-1">1 Bathroom</p>
           <p className="bg-secondaryGreen rounded px-2 py-1">600 sqft</p>
         </div>
-      </div>
+      </section>
 
       <section className="grid lg:grid-cols-12 gap-10 pt-8 pb-16">
-        <div className="flex flex-col gap-3 p-3 lg:col-span-6">
+        <div className="flex flex-col p-3 lg:col-span-6">
           <h4 className="col-span-full text-3xl pb-4 font-medium">Amenities</h4>
           <ul className="flex flex-wrap gap-3">
             {lockwood725.amenities.map((item, index) => (
@@ -29,10 +29,11 @@ export default function Lockwood725Page() {
             ))}
           </ul>
         </div>
-        <div className="flex flex-col gap-3 lg:col-span-6">
+
+        <section className="flex flex-col lg:col-span-6 p-3">
           <h4 className="col-span-full text-3xl pb-4 font-medium">Photos</h4>
           <Carousel photos={lockwood725.photos} />
-        </div>
+        </section>
       </section>
     </div>
   );
