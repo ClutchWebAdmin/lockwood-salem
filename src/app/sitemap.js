1b436466d0ec5e39a3f7c690f4385a4b80cdfd2d
Gabe Wolford <gabewolford@gmail.com>
2024-07-02 10:15:51 -0700

    added sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,22 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://lockwoodsalem.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://lockwoodsalem.com/701-723-lockwood",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://lockwoodsalem.com/725-lockwood",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
